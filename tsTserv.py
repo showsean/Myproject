ca66727a9c7fb1f36f969dcb056056e646ba4283
sean <gxx@live.com>
2017-03-02 13:18:54 +0800

    My git commit command practice

---
+++ tsTserv.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+
+from socket import *
+from time import ctime
+
+HOST = ''
+PORT = 21567
+BUFSIZE = 1024
+ADDR = (HOST,PORT)
+
+tcpSerSock = socket(AF_INET,SOCK_STREAM)
+tcpSerSock.bind(ADDR)
+tcpSerSock.listen(5)
+
+
+try:
+    while True :
+        print 'Waiting for connection...'
+        tcpCliSock,addr = tcpSerSock.accept()
+        print '...connected from :',addr
+    
+        while True:
+            data = tcpCliSock.recv(BUFSIZE)
+            if not data:
+                break
+            tcpCliSock.send('[%s] %s'%(ctime(),data))
+            tcpCliSock.close()
+except Exception ,diag:
+    #print diag
+    tcpSerSock.close()
